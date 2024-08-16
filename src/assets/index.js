2ca5a82c95de4f071b2204f95d34aad687aa7324
zaidlove <loveyours0214@gmail.com>
2024-08-16 08:41:18 -0700

    "Fixed: to delete logo.png"

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,3 @@
-import logo from "./logo.png";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -35,7 +34,6 @@ import tripguide from "./tripguide.png";
 
 export {
   gdsc,
-  logo,
   backend,
   creator,
   mobile,
